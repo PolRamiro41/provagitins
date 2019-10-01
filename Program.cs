6f00382cb83e1557abe85669bd75073ea97326fa
Pol Ram <polram@gmail.com>
2019-10-01 17:01:59 +0200

    Creem menu sense opcions

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JOCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcio =0;
+            do
+            {
+                Console.WriteLine("MENU");
+
+                opcio = int.Parse(Console.ReadLine());
+            }
+            while (opcio != 0);
+        }
+    }
+}
